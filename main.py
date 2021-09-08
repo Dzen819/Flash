5db330f3d9b5ed9d21fa62da0bc929b76e9415c3
Dzen819 <43151543+Dzen819@users.noreply.github.com>
2021-09-08 13:48:12 +0300

    3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import pandas
 import random
+
 BACKGROUND_COLOR = "#B1DDC6"
 try:
     words_frame = pandas.read_csv("./data/words_to_learn.csv")
@@ -20,24 +21,30 @@ def random_word():
     canvas.itemconfigure(background_img, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
 
+
 def flip_card():
     canvas.itemconfigure(en_text, text=current_word["Russian"], fill="white")
     canvas.itemconfigure(title, text="Russian", fill="white")
     canvas.itemconfigure(background_img, image=card_back_img)
+
+
 def yes():
     words_dict.remove(current_word)
     new_words = pandas.DataFrame(words_dict)
     new_words.to_csv("data/words_to_learn.csv", index=False)
     random_word()
+
+
 def no():
     random_word()
 
-#------------------------------------WINDOW---------------------------------------#
+
+# ------------------------------------WINDOW---------------------------------------#
 window = Tk()
 window.title("Flash")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(3000, func=flip_card)
-#------------------------------------CANVAS---------------------------------------#
+# ------------------------------------CANVAS---------------------------------------#
 canvas = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="./images/card_front.png")
 card_back_img = PhotoImage(file="./images/card_back.png")
@@ -45,7 +52,7 @@ background_img = canvas.create_image(400, 263, image=card_front_img)
 title = canvas.create_text(400, 150, text="English", font=("Ariel", 40, "italic"))
 en_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
-#------------------------------------BUTTONS---------------------------------------#
+# ------------------------------------BUTTONS---------------------------------------#
 yes_img = PhotoImage(file="./images/right.png")
 no_img = PhotoImage(file="./images/wrong.png")
 yes_button = Button(image=yes_img, highlightthickness=0, command=yes)
@@ -54,24 +61,5 @@ yes_button.grid(column=0, row=1)
 no_button.grid(column=1, row=1)
 
 random_word()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.mainloop()

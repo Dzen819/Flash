1ccb95cf7b0e731ac53735089ee97a8e79e8d7c2
Dzen819 <43151543+Dzen819@users.noreply.github.com>
2021-09-08 12:22:38 +0300

    1

--- main.py
+++ main.py
@@ -4,35 +4,45 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 
 words_frame = pandas.read_csv("./data/words.csv")
+words_dict = words_frame.to_dict(orient="records")
+current_word = {}
 
 
 def random_word():
-    r = random.randint(1, len(words_frame))
-    ru_word = words_frame["Russian"][r]
-    en_word = words_frame["English"][r]
-    return [ru_word, en_word]
+    global current_word, flip_timer
+    window.after_cancel(flip_timer)
+    current_word = random.choice(words_dict)
+    canvas.itemconfigure(en_text, text=current_word["English"], fill="black")
+    canvas.itemconfigure(title, text="English", fill="black")
+    canvas.itemconfigure(background_img, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+def flip_card():
+    canvas.itemconfigure(en_text, text=current_word["Russian"], fill="white")
+    canvas.itemconfigure(title, text="Russian", fill="white")
+    canvas.itemconfigure(background_img, image=card_back_img)
+
 #------------------------------------WINDOW---------------------------------------#
 window = Tk()
 window.title("Flash")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 #------------------------------------CANVAS---------------------------------------#
 canvas = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="./images/card_front.png")
 card_back_img = PhotoImage(file="./images/card_back.png")
-canvas.create_image(400, 263, image=card_front_img)
-canvas.create_image(400, 263, image=card_back_img)
-canvas.create_text(400, 150, text="English", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text=random_word()[1], font=("Ariel", 60, "bold"))
+background_img = canvas.create_image(400, 263, image=card_front_img)
+title = canvas.create_text(400, 150, text="English", font=("Ariel", 40, "italic"))
+en_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 #------------------------------------BUTTONS---------------------------------------#
 yes_img = PhotoImage(file="./images/right.png")
 no_img = PhotoImage(file="./images/wrong.png")
-yes_button = Button(image=yes_img, highlightthickness=0)
-no_button = Button(image=no_img, highlightthickness=0)
+yes_button = Button(image=yes_img, highlightthickness=0, command=random_word)
+no_button = Button(image=no_img, highlightthickness=0, command=random_word)
 yes_button.grid(column=0, row=1)
 no_button.grid(column=1, row=1)
 
-
+random_word()
 
 
 
